8a2772565d758e7a1ede8df0b222930440e959bc
Vadim-Kostetskyi <rasty2308@gmail.com>
2025-06-04 21:01:34 +0300

    ap

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGO_URI!);
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
